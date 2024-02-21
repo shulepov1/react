02d306526c128f9f84d45498e89c48bd88086873
shulepov1 <ilia.s12@yandex.ru>
2024-02-21 20:57:24 +0300

    lab1

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import RecursiveComponent from "./components/RecursiveComponent";
+import "./App.css";
+
+function App() {
+    return (
+        <main>
+            <RecursiveComponent colorIndex={0}></RecursiveComponent>
+        </main>
+    );
+}
+
+export default App;
---
+++ src/components/RecursiveComponent.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import "./RecursiveComponent.css";
+
+type Props = {
+    colorIndex: number;
+    depth: number;
+};
+
+export default function RecursiveComponent({
+    colorIndex = 0,
+    depth = 1,
+}: Props) {
+    const [childrenCount, setChildrenCount] = useState(0);
+    const colors = [
+        "rgb(255, 0, 0",
+        "rgb(255, 165, 0",
+        "rgb(255, 255, 0",
+        "rgb(0, 128, 0",
+        "rgb(0, 0, 255",
+        "rgb(75, 0, 130",
+        "rgb(238, 130, 238",
+    ];
+
+    const nextColorIndex = (colorIndex + 1) % colors.length;
+    const borderColor = colors[colorIndex] + ")";
+    const backgroundColor = colors[colorIndex] + ", 0.25)";
+
+    const handleClick = () => {
+        if (depth >= 15) {
+            alert("Stop it!");
+            return;
+        }
+        const newChildrenCount = childrenCount + 1;
+        setChildrenCount(newChildrenCount);
+    };
+
+    return (
+        <div
+            className="component"
+            style={{ backgroundColor, border: `3px solid ${borderColor}` }}
+        >
+            <p className="name">[children: {childrenCount}]</p>
+            <button className="addChildButton" onClick={handleClick}>
+                Add Child
+            </button>
+            <div className="children">
+                {Array(childrenCount)
+                    .fill(null)
+                    .map((child) => {
+                        return (
+                            <RecursiveComponent
+                                colorIndex={nextColorIndex}
+                                depth={depth + 1}
+                            ></RecursiveComponent>
+                        );
+                    })}
+            </div>
+        </div>
+    );
+}
